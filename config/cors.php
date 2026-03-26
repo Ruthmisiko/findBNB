4e7928d905a7e9b625561e12d77cf643bc86f1ae
Ruthmisiko <ruthmisiko2@gmail.com>
2026-03-26 13:03:32 -0400

    push

--- config/cors.php
+++ config/cors.php
@@ -17,18 +17,20 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+'allowed_origins' => [
+    'https://findbnb.netlify.app',
+],
 
-    'allowed_origins_patterns' => [],
+'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+'exposed_headers' => [],
 
-    'max_age' => 0,
+'max_age' => 0,
 
-    'supports_credentials' => false,
+'supports_credentials' => true,
 
 ];
